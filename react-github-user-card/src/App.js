6f4a7a37d99ddec440cbdea3cb51eb63e9d362ca
HannahMarieWieser <nikauli@gmail.com>
2019-08-06 17:39:31 -0400

    basic rendering cards

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import userCard from './Components/userCard'
+import UserCard from './Components/UserCard'
 import axios from 'axios'
 import './App.css';
+import FollowersCard from './Components/FollowersCard'
 
 class App extends React.Component{
 
   constructor(){
     super();
     this.state = {
-      users: []
-    }
+      users: [],
+      followers: []
+    };
     
   }
 
   componentDidMount(){
     this.userInfo();
-    
+
+    axios.get('https://api.github.com/users/hannahmariewieser/followers')
+    .then(follower => {
+        this.setState({ followers : follower.data})
+        console.log("follower", follower.data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
   }
 
 
@@ -36,13 +47,16 @@ class App extends React.Component{
 
 
   render(){
-    console.log("state-users", this.state.users)
+    //console.log("state-users", this.state.users)
+    console.log("state-followers", this.state.followers)
     return(
       <div>
         <h1>React Github UserCards</h1>
+        <UserCard key = {this.state.users} info = {this.state.users}/>
 
-        {this.state.users.map(user => {
-          return <userCard key={user} info = {user}/>
+        {this.state.followers.map(follow => {
+          console.log("follow", follow)
+          return <FollowersCard key={follow} info = {follow}/>
         })}
 
       </div> 
--- react-github-user-card/src/Components/FollowersCard.js
+++ react-github-user-card/src/Components/FollowersCard.js
@@ -3,20 +3,20 @@ import { Card, Icon, Image } from 'semantic-ui-react'
 
 
 
-function UserCard (props) {
+function FollowersCard (props) {
     console.log("props", props)
     return <Card>
         <Image src= {props.info.avatar_url} wrapped ui={false} />
         <Card.Content>
         <Card.Header>{props.info.login}</Card.Header>
         <Card.Meta>
-            <span className='date'>Followers: {props.info.followers}</span>
+            <span className='date'>Followers: {props.info.followers_url}</span>
         </Card.Meta>
         <Card.Meta>
-            <span className='date'>Following: {props.info.following}</span>
+            <span className='date'>Following: {props.info.following_url}</span>
         </Card.Meta>
         <Card.Meta>
-            <span className='date'>Public Repos: {props.info.public_repos}</span>
+            <span className='date'>Public Repos: {props.info.repos_url}</span>
         </Card.Meta>
         </Card.Content>
     </Card>
@@ -24,4 +24,4 @@ function UserCard (props) {
 
 
 
-export default UserCard;
+export default FollowersCard;
